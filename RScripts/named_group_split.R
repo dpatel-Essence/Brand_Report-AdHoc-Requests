f52ba0e88b68f765b6cd63f814f6cb0f9011059f
dpatel-essence <darshan.patel@essenceglobal.com>
2022-08-04 13:29:14 -0400

    Initial Commit

--- RScripts/named_group_split.R
+++
@@ -1,8 +0,0 @@
-named_group_split <- function(.tbl, ...) {
-  grouped <- group_by(.tbl, ...)
-  names <- rlang::inject(paste(!!!group_keys(grouped), sep = " / "))
-  
-  grouped %>% 
-    group_split() %>% 
-    rlang::set_names(names)
-}
--- RScripts/names_function.R
+++
@@ -1,8 +0,0 @@
-names_function <- function(.tbl, ...) {
-grouped <- group_by(.tbl, ...)
-names_split <- rlang::inject(paste(!!!group_keys(grouped), sep = " / "))
-return(names_split)
-}
-
-
-
